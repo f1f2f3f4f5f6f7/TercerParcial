5e6d832c81f71c9d1636625f5a410f87960aa205
Santiago Gomez Villarreal <santaigogv0506@gmail.com>
2023-11-30 20:29:28 -0500

    correcciones generales

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,7 +5,7 @@ from django import forms
 class NotasForm(ModelForm):
     class Meta:
         model= Notas
-        fields = ['title','datecompleted', 'nota']
+        fields = ['title', 'nota']
         widgets = {
             'datecompleted': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
         }
--- tasks/models.py
+++ tasks/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 # Create your models here.
 
 class Notas(models.Model):
     title= models.CharField(max_length=150)
     created= models.DateTimeField(auto_now_add=True)
     datecompleted= models.DateTimeField(null = True)
-    nota=models.FloatField(default=0.0)
+    nota=models.CharField(max_length=200)
     user= models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
